9abb1441ae5a189976d9efb7bb769a66199925f3
David Martin <demartins37@yahoo.com>
2019-07-22 14:04:20 -0400

    do not merge

--- assets/logic.js
+++ assets/logic.js
@@ -1,43 +1,45 @@
-
 $("#submitButton").on("click", function(event){
   event.preventDefault();
-    $("#box").empty;
   var ingredient = $("#ingredientsInput").val();
+//  var diet = "none";
+  var diet = $("#dietInput").val();
+// dropdown menu or input with 6 options only
+  diet ="balanced"
+//  var health = "none";
+  var health = $("#healthInput").val();
+// dropdown menu or input field with 4 options only
+ health ="alcohol-free"
+  var filter1 = "&diet=";
+  var filter2 = "&health=";
 
 
 
-
-// function recipeSearch(){  
+ 
 var appKey = "eb86cdb3f13f6d7cd006d3e874c6754c";
 var appId = "faf106fd";
 // var appKey = "96e1271b0342c1d83247134c2a36f068";
 // var appId = "7351fa17";
 //the API ID = 7351fa17. and the API key = 96e1271b0342c1d83247134c2a36f068
-var queryUrl = "https://api.edamam.com/search?q=" + ingredient + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=6";
-//"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
-//“cuisineType=chinese&cuisineType=indian”
+var queryUrl = "https://api.edamam.com/search?q=" + ingredient + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=4";
+if(diet !== "none"){
+  queryUrl += filter1 + diet
+}
+if(health !== "none"){
+  queryUrl += filter2 + health
+}
+//var queryUrl2 = "https://api.edamam.com/search?q=" + ingredient + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=4" + filter1 + diet + filter2 + health;
+//var queryUrl3 = "https://api.edamam.com/search?q=" + ingredient + "&app_id=" + appId + "&app_key=" + appKey + "&from=0&to=4&diet=low-fat&health=alcohol-free"; 
+
 console.log(queryUrl);
-  $.ajax({
+
+$.ajax({
     url: queryUrl,
     method: "GET"
   }).then(function(response){
-    // console.log(response);
-    // console.log(response.hits);
-    // console.log(response.hits[0]);
-    // console.log(response.hits[0].recipe);
-    // console.log(response.hits[0].recipe.label);
-    // console.log(response.hits[0].recipe.url);
-    // console.log(response.hits[0].recipe.image);
-
-    // console.log(response.hits[1].recipe.label);
-    // console.log(response.hits[1].recipe.url);
-    // console.log(response.hits[1].recipe.image);
-
-    // console.log(response.hits[2].recipe.label);
-    // console.log(response.hits[2].recipe.url);
-    // console.log(response.hits[2].recipe.image);
+    
     
     for (var i = 0; i < response.hits.length; i++) {
+      console.log(response);
       console.log(response.hits[i].recipe.label);
       var label = response.hits[i].recipe.label;
       console.log(response.hits[i].recipe.url);
@@ -45,6 +47,10 @@ console.log(queryUrl);
       url.text("Click Here to View")
       console.log(response.hits[i].recipe.image);
       var image = $("<img style='width:100%' src=" + response.hits[i].recipe.image + ">");
+      var totalFat = " Total Fat" + ": "  + Math.round(response.hits[i].recipe.totalNutrients.FAT.quantity) + " grams ";
+      var totalCarb = "Total Carb" + ": "  + Math.round(response.hits[i].recipe.totalNutrients.CHOCDF.quantity) + " grams ";
+      var totalProtein = "Total Protein" + ": " + Math.round(response.hits[i].recipe.totalNutrients.PROCNT.quantity) + " grams";
+      
 
       var newDiv = $("<div class=col-lg-4 style=display:inline-block id=recipe></div>");
 
@@ -53,8 +59,8 @@ console.log(queryUrl);
 
 
       
-      newDiv.text(label)
-      newDiv.append(image, url, newButton)
+      newDiv.text(label,)
+      newDiv.append(image, url, newButton, totalFat, totalCarb, totalProtein)
       newDiv.css({
           "padding": "20px",
           "border": "solid black 1px",
@@ -79,7 +85,7 @@ console.log(queryUrl);
 
 
     }     
-  });  
+ });  
 
     
 });
